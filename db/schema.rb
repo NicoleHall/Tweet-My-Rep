3e950bfa1e8400d6af0e1cdbc00e4348a21bf18b
Nicole Hall <nicolealexandrahall@gmail.com>
2016-01-19 14:07:29 -0700

    added avatar jpg to User db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119033539) do
+ActiveRecord::Schema.define(version: 20160119210104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160119033539) do
     t.string   "oauth_token_secret"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "avatar"
   end
 
 end
