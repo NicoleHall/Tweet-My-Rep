744c99bc5082a43569fd04b2d8392d10ac5af191
Nicole Hall <nicolealexandrahall@gmail.com>
2016-01-19 14:27:02 -0700

    added nyt service and can parse results, limit 3 articles

--- app/services/nyt_service.rb
+++ app/services/nyt_service.rb
@@ -1,13 +1,24 @@
 class NytService
-
   include HTTParty
-  base_uri 'http://api.nytimes.com/svc/search/v2/articlesearch.json'
+  base_uri 'http://api.nytimes.com'
+
+  ARTICLE_LIMIT = 3
+
 
   def initialize
-    @options = { apikey: ENV["sunlight_api_key"] }
+    @options = { "api-key": ENV["article_search_api_key"] }
   end
 
-  def search
-    self.class.get("q=#{Legislator.full_name}", @options)
+  def search_articles(legislator)
+    response = self.class.get("/svc/search/v2/articlesearch.json",
+               query: @options.merge(q: legislator.full_name) )
+    parse_articles(response)
+  end
+
+  private
+  def parse_articles(response)
+    response.parsed_response["response"]["docs"].take(ARTICLE_LIMIT).map do |article_hash|
+      article_hash.slice("web_url", "snippet")
+    end
   end
 end
