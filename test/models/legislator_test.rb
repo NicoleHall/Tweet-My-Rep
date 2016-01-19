f2b5f5e27bb67c7a4aa29bf60346d46cc4b79668
Nicole Hall <nicolealexandrahall@gmail.com>
2016-01-19 13:03:13 -0700

    changed legislator model tests that test @ symbol is added and first and last name are joined using the full_name method

--- test/models/legislator_test.rb
+++ test/models/legislator_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class LegislatorTest < ActiveSupport::TestCase
   test "twitter_handle returns the twitter id with an at sign" do
-    assert_equal "@sgrif", Legislator.new(twitter_id: "sgrif").twitter_handle
-    assert_equal "@timcook", Legislator.new(twitter_id: "timcook").twitter_handle
+    sean = Legislator.create(twitter_id: "sgrif", first_name: "Sean", last_name: "Griffin")
+    tim  = Legislator.create(twitter_id: "timcook", first_name: "Tim", last_name: "Cook")
+
+    assert_equal "@sgrif", sean.twitter_id
+    assert_equal "@timcook", tim.twitter_id
   end
 
   test "full_name returns the first and last name" do
-    assert_equal "Sean Griffin", Legislator.new(first_name: "Sean", last_name: "Griffin").full_name
-    assert_equal "Tim Cook", Legislator.new(first_name: "Tim", last_name: "Cook").full_name
+    sean = Legislator.create(twitter_id: "sgrif", first_name: "Sean", last_name: "Griffin")
+    tim  = Legislator.create(twitter_id: "timcook", first_name: "Tim", last_name: "Cook")
+
+    assert_equal "Sean Griffin", sean.full_name
+    assert_equal "Tim Cook", tim.full_name
   end
 end
