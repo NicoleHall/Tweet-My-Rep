9024db36e262267cbc9bd20fac2cb815f0061908
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-14 14:19:05 -0600

    random zipcode works, but controller test is weird

--- app/controllers/random_zipcodes_controller.rb
+++ app/controllers/random_zipcodes_controller.rb
@@ -3,5 +3,6 @@ class RandomZipcodesController < ApplicationController
 
   def index
     @random_zip = Zipcode.get_a_random_zip
+      #  require 'pry'; binding.pry
   end
 end
--- test/controllers/random_zipcodes_controller_test.rb
+++ test/controllers/random_zipcodes_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RandomZipcodesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#index returns a random zipcode record' do
+    VCR.use_cassette('localhost')do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:random_zip)
+    end
+  end
 end
