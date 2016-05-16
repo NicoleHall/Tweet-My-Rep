b21228f652fd5750aaa596ce30c33af5234e356b
Nicole Hall <nicolealexandrahall@gmail.com>
2016-05-16 15:10:06 -0600

    NYT articles are now sorted by publish date

--- app/services/nyt_service.rb
+++ app/services/nyt_service.rb
@@ -11,14 +11,17 @@ class NytService
   def search_articles(legislator)
     response = self.class.get("/svc/search/v2/articlesearch.json",
                query: @options.merge(q: legislator.full_name) )
-      
     parse_articles(response)
   end
 
   private
   def parse_articles(response)
-    response.parsed_response["response"]["docs"].take(ARTICLE_LIMIT).map do |article_hash|
-      article_hash.slice("web_url", "snippet")
+    three_articles = response.parsed_response["response"]["docs"].take(ARTICLE_LIMIT).map do |article_hash|
+      article_hash.slice("web_url", "snippet", "pub_date")
     end
+    three_articles.sort_by do |article|
+      article["pub_date"].to_datetime
+    end.reverse
   end
+  
 end
